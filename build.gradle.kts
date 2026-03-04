d764e70a4cb5337d237bc564328941837724d684
Musca Mihail <muscamihaill@gmail.com>
2026-03-04 23:27:53 +0200

    fix: build.gradle.kts & publish workflow?

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,8 @@ val isCI = System.getenv("GITHUB_ACTIONS") == "true"
 // }
 
 java {
+    withSourcesJar()
+    withJavadocJar()
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
@@ -44,12 +46,12 @@ dependencies {
 
 publishing {
     publications {
-        create<MavenPublication>("mavenJava") {
+        create<MavenPublication>("maven") {
             from(components["java"])
             
             pom {
                 name.set("Fluff Loader")
-                description.set("A concise description of what it does")
+                description.set("A brief description of your library")
                 url.set("https://github.com/muscaa/fluff-loader")
                 licenses {
                     license {
@@ -57,43 +59,35 @@ publishing {
                         url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                     }
                 }
-                // developers {
-                //     developer {
-                //         id.set("yourusername")
-                //         name.set("Your Name")
-                //     }
-                // }
-                // scm {
-                //     connection.set("scm:git:git://github.com/username/repo.git")
-                //     developerConnection.set("scm:git:ssh://github.com/username/repo.git")
-                //     url.set("https://github.com/username/repo")
-                // }
+                developers {
+                    developer {
+                        id.set("muscaa")
+                        name.set("Musca")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/muscaa/fluff-loader.git")
+                    url.set("https://github.com/muscaa/fluff-loader")
+                }
             }
         }
     }
 
     repositories {
         maven {
-            // "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val stagingRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotRepoUrl else stagingRepoUrl)
-            
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
+                username = System.getenv("CENTRAL_TOKEN_USERNAME")
+                password = System.getenv("CENTRAL_TOKEN_PASSWORD")
             }
         }
     }
 }
 
 signing {
-    if (isCI) {
-        val key = System.getenv("MAVEN_GPG_KEY")
-        val passphrase = System.getenv("MAVEN_GPG_PASSPHRASE")
-        useInMemoryPgpKeys(key, passphrase)
-    } else {
-        useGpgCmd()
-    }
-    sign(publishing.publications["mavenJava"])
+    val signingKey = System.getenv("GPG_SIGNING_KEY")
+    val signingPassword = System.getenv("GPG_SIGNING_KEY_PASSWORD")
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["maven"])
 }
